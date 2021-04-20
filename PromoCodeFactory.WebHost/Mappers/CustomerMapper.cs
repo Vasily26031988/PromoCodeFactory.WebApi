98284e50a34f3e6feeb7cd451d58a3cb0235acb5
Vasily Gololobov <Sator1988@gmail.com>
2021-04-20 13:16:12 +0400

    Create Mappers

--- PromoCodeFactory.WebHost/Mappers/CustomerMapper.cs
+++ PromoCodeFactory.WebHost/Mappers/CustomerMapper.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Models;
 
 namespace PromoCodeFactory.WebHost.Mappers
 {
     public class CustomerMapper
     {
+	    public static Customer MapFromModel(CreateOrEditCustomerRequest request, IEnumerable<Preference> preference,
+		    Customer customer = null)
+	    {
+		    customer ??= new Customer
+		    {
+			    Preferences = new List<CustomerPreference>(),
+			    PromoCodes = new List<PromoCodeCustomer>(),
+		    };
+
+		    customer.FirstName = request.FirstName;
+		    customer.LastName = request.LastName;
+		    customer.Email = request.Email;
+
+			customer.Preferences?.Clear();
+			if (preference?.Any() == false)
+				return customer;
+
+			customer.Preferences = preference
+				.Select(x => new CustomerPreference(customer, x))
+				.ToList();
+
+			return customer;
+	    }
     }
 }
--- PromoCodeFactory.WebHost/Mappers/PromoCodeMapper.cs
+++ PromoCodeFactory.WebHost/Mappers/PromoCodeMapper.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Models;
 
 namespace PromoCodeFactory.WebHost.Mappers
 {
     public class PromoCodeMapper
     {
+	    public static PromoCode MapFromModel(
+		    GivePromoCodeRequest request,
+		    Preference preference,
+		    IEnumerable<Customer> customers)
+	    {
+		    var promoCode = new PromoCode
+		    {
+			    Code = request.PromoCode,
+			    ServiceInfo = request.ServiceInfo,
+			    PartnerName = request.PartnerName,
+			    BeginDate = DateTime.Today,
+			    EndDate = DateTime.Today.AddDays(30),
+				Preference = preference,
+				Customers = new List<PromoCodeCustomer>(),
+		    };
+
+		    if (customers?.Any() == false)
+			    return promoCode;
+
+		    promoCode.Customers = customers
+			    .Select(x => new PromoCodeCustomer(promoCode, x))
+			    .ToList();
+
+
+			return promoCode;
+		}
     }
 }
